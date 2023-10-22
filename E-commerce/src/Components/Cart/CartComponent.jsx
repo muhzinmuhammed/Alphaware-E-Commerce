60ec531db22ea8befea2c7dd52b29e593043090c
Muhzin <muhzinsidhiq333@gmail.com>
2023-10-22 11:32:20 -0400

    Implement search and pagination in product view Page

--- E-commerce/src/Components/Cart/CartComponent.jsx
+++ E-commerce/src/Components/Cart/CartComponent.jsx
@@ -1,6 +1,7 @@
 import { Container, Table } from "react-bootstrap";
 import "./Cart.css";
-
+import Swal from "sweetalert2";
+import cartEmpthy from "../../assets/images.png";
 import { useState } from "react";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -24,13 +25,12 @@ function CardComponent() {
         updatedCounts[id] = productCounts[id] - 1;
         setProductCounts(updatedCounts);
         localStorage.setItem(`product_${id}`, updatedCounts[id].toString());
-        toast.success("Product  Decrease Suuccessfully");
+        toast.success("Product Decrease Successfully");
       }
     } catch (error) {
       toast.error(error);
     }
   };
-  /* Decrease product  Qty*/
 
   /* Increase Product Qty*/
   const handleIncrement = (id) => {
@@ -44,74 +44,108 @@ function CardComponent() {
       toast.error(error);
     }
   };
-  /* Increase Product Qty*/
 
-  /* Remove Product Frmo Cart*/
+  /* Remove Product From Cart*/
   const handleRemove = (id) => {
-    try {
-      const existingCartData = JSON.parse(
-        localStorage.getItem("cartDataProduct")
-      );
-      const newData = existingCartData.filter((item) => {
-        return item?.product?._id !== id;
-      });
-      localStorage.setItem("cartDataProduct", JSON.stringify(newData));
-      window.location.reload();
-    } catch (error) {
-      toast.error(error);
-    }
+    // Show a SweetAlert confirmation dialog
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You are about to remove this product from your cart.",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, remove it",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        try {
+          const existingCartData = JSON.parse(
+            localStorage.getItem("cartDataProduct")
+          );
+          const newData = existingCartData.filter(
+            (item) => item?.product?._id !== id
+          );
+          localStorage.setItem("cartDataProduct", JSON.stringify(newData));
+          // Show toast message before reloading the page
+          toast.success("Product remove from cart");
+          // Reload the page
+          window.location.reload();
+        } catch (error) {
+          toast.error(error);
+        }
+      }
+    });
   };
-   /* Remove Product Frmo Cart*/
-   
+
   return (
     <Container className="cart-page">
       <ToastContainer />
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Product Image</th>
-            <th>Product Price</th>
-            <th>Qunatity</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {ProductData.map((product, index) => (
-            <tr key={index}>
-              <td>{index + 1}</td>
-              <td>
-                <img style={{ width: "80px" }} src={product.product.imageUrl} />
-              </td>
-              <td>{product?.product?.discountAmount}</td>
-              <td>
-                <button
-                  className="cart-button text-center btn btn-info text-white rounded"
-                  onClick={() => handleDecrement(product?.product?._id)}
-                >
-                  -
-                </button>
-                &nbsp;&nbsp;
-                {productCounts[product?.product?._id]}&nbsp;&nbsp;
-                <button
-                  className="cart-button btn-info btn btn-info text-white rounded"
-                  onClick={() => handleIncrement(product?.product?._id)}
-                >
-                  +
-                </button>
-              </td>
-              <td>
-                <button
-                  className="btn btn-danger "
-                  onClick={() => handleRemove(product?.product?._id)}
-                >
-                  Remove
-                </button>
-              </td>
+      {ProductData.length > 0 ? (
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Product Image</th>
+              <th>Product Price</th>
+              <th>Qunatity</th>
+              <th>Action</th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {ProductData.map((product, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>
+                  <img
+                    style={{ width: "80px" }}
+                    src={product.product.imageUrl}
+                    alt="Product"
+                  />
+                </td>
+                <td>{product?.product?.discountAmount}</td>
+                <td>
+                  <button
+                    className="cart-button text-center btn btn-info text-white rounded"
+                    onClick={() => handleDecrement(product?.product?._id)}
+                  >
+                    -
+                  </button>
+                  &nbsp;&nbsp;
+                  {productCounts[product?.product?._id]}&nbsp;&nbsp;
+                  <button
+                    className="cart-button btn-info btn btn-info text-white rounded"
+                    onClick={() => handleIncrement(product?.product?._id)}
+                  >
+                    +
+                  </button>
+                </td>
+                <td>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => handleRemove(product?.product?._id)}
+                  >
+                    Remove
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      ) : (
+        <>
+          <Container className="ms-5">
+            <img src={cartEmpthy} />
+            <h1 className="  text-info">No Product In Cart </h1>
+            <button
+              href="/"
+              onClick={() => (window.location.href = "/")}
+              className=" btn btn-light text-info"
+            >
+              Continue Shopiing
+            </button>
+          </Container>
+        </>
+      )}
     </Container>
   );
 }
--- E-commerce/src/Components/Product/productComponent.jsx
+++ E-commerce/src/Components/Product/productComponent.jsx
@@ -3,19 +3,45 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
-
+import "./Product.css";
 
 const ProductComponent = () => {
   const [products, setProducts] = useState([]); // Changed the variable name to 'products'.
-  
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 6;
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const filteredProducts = products.filter((product) =>
+    product.description.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const totalPages = Math.ceil(filteredProducts.length / itemsPerPage);
+
+  const indexOfLastProduct = currentPage * itemsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - itemsPerPage;
+  const currenProduct = filteredProducts.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
   useEffect(() => {
     const fetchProductData = async () => {
       try {
-        const productResponse = await axios.post( // Use 'get' instead of 'post' for fetching data.
+        const productResponse = await axios.post(
+          // Use 'get' instead of 'post' for fetching data.
           "http://3.7.252.58:4001/product/getAllProduct"
         );
-        console.log(productResponse.data, "lll");
+
         setProducts(productResponse.data);
       } catch (error) {
         console.error("Error fetching product data:", error);
@@ -25,16 +51,23 @@ const ProductComponent = () => {
     fetchProductData();
   }, []);
 
-  const addToCart = (product) => { // Changed the function name to follow naming conventions.
+  const addToCart = (product) => {
+    // Changed the function name to follow naming conventions.
     try {
-      const existingCartData = JSON.parse(localStorage.getItem("cartDataProduct")) || [];
+      const existingCartData =
+        JSON.parse(localStorage.getItem("cartDataProduct")) || [];
 
-      const productExistsInCart = existingCartData.some((item) => item.product._id === product._id);
+      const productExistsInCart = existingCartData.some(
+        (item) => item.product._id === product._id
+      );
 
       if (!productExistsInCart) {
         toast.success("Product added to cart successfully");
         existingCartData.push({ product });
-        localStorage.setItem("cartDataProduct", JSON.stringify(existingCartData));
+        localStorage.setItem(
+          "cartDataProduct",
+          JSON.stringify(existingCartData)
+        );
       } else {
         toast.error("Product already exists in the cart");
       }
@@ -48,11 +81,25 @@ const ProductComponent = () => {
       <ToastContainer />
       <h1 className="text-center mt-3">Product List</h1> {/* Added a title. */}
       <section className="py-4 container product-view">
-        <div className="row justify-content-center">
-          {products.map((productItem) => (
-            <div key={productItem._id} className="col-11 col-md-6 col-lg-3 mx-0 mb-4">
+        <input
+          type="text"
+          placeholder="Search for a Products..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className="form-control"
+        />
+        <div className="row mt-5 justify-content-center">
+          {currenProduct.map((productItem) => (
+            <div
+              key={productItem._id}
+              className="col-11 col-md-6 col-lg-4 mx-0 mb-4"
+            >
               <div className="card p-0 overflow-hidden h-100 shadow">
-                <img className="card-img-top img-fluid" src={productItem.imageUrl} alt="Card image cap" />
+                <img
+                  className="card-img-top img-fluid"
+                  src={productItem.imageUrl}
+                  alt="Card image cap"
+                />
                 <div className="card-body text-center">
                   <h5 className="card-title">{productItem.description}</h5>
                   <h6 className="text-center text-danger text-decoration-line-through ">
@@ -62,11 +109,39 @@ const ProductComponent = () => {
                     OFFER : ₹ {productItem.discountAmount}
                   </h2>
 
-                  <button onClick={() => addToCart(productItem)} className="btn btn-info text-white">Add To Cart</button> 
+                  <button
+                    onClick={() => addToCart(productItem)}
+                    className="add-to-cart"
+                  >
+                    Add To Cart
+                  </button>
                 </div>
               </div>
             </div>
           ))}
+        </div>
+        <div className="pagination">
+          {currentPage > 1 && (
+            <button onClick={handlePrevPage} className="pagination-button">
+              Prev
+            </button>
+          )}
+          {Array.from({ length: totalPages }, (_, index) => (
+            <button
+              key={index}
+              onClick={() => setCurrentPage(index + 1)}
+              className={`pagination-button ${
+                currentPage === index + 1 ? "active" : ""
+              }`}
+            >
+              {index + 1}
+            </button>
+          ))}
+          {currentPage < totalPages && (
+            <button onClick={handleNextPage} className="pagination-button">
+              Next
+            </button>
+          )}
         </div>
       </section>
     </>
